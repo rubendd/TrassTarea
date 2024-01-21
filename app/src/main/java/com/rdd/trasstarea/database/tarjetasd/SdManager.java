8f1de0f58e26519b1aed9ec5f643c0ec12463f59
rubendd <rubeendugo2003@gmail.com>
2024-01-21 12:37:37 +0100

    Borrado de archivos

--- app/src/main/java/com/rdd/trasstarea/database/tarjetasd/SdManager.java
+++ app/src/main/java/com/rdd/trasstarea/database/tarjetasd/SdManager.java
@@ -5,33 +5,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.rdd.trasstarea.R;
-import com.rdd.trasstarea.model.Task;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.List;
 
 public class SdManager {
 
